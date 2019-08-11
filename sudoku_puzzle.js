f49b69b1376c57749a1131b248101b5e05d67828
Your Name <you@example.com>
2019-08-11 17:09:44 +0530

    update sudoku_puzzle.js file

--- sudoku_puzzle.js
+++ sudoku_puzzle.js
@@ -1,16 +1,17 @@
+// puzzle solver start from here.
 function solveSudoku(gameArr) {
     var emptySpot = nextEmtySpot(gameArr);
     var r = emptySpot[0];
     var c = emptySpot[1];
 
-    if (!isValidSudoku(gameArr)) return gameArr;
+    // if (!isValidSudoku(gameArr)) return gameArr;
 
     if (r === -1) {
         return gameArr;
     };
 
     var possArr = possiblities(r, c, gameArr);
-
+// this loop for store the possiable value in Array.
     for (var k = 0; k < possArr.length && nextEmtySpot(gameArr)[0] !== -1; k++) {
         gameArr[r][c] = possArr[k];
         solveSudoku(gameArr);
@@ -39,19 +40,21 @@ function possiblities(r, c, gameArr) {
     var l = 0;
     if (r <= 2) k = 0; else if (r <= 5) k = 3; else k = 6;
     if (c <= 2) l = 0; else if (c <= 5) l = 3; else l = 6;
-
+// this loop for rows 3*3 grid.
     for (var i = 0; i < 9; i++) {
         row.push(gameArr[i][c]);
     }
+// this loop for columns 3*3 grid.    
     for (var j = 0; j < 9; j++) {
         col.push(gameArr[r][j]);
     }
+// this loop for columns 3*3 grid.    
     for (var i = k; i < k + 3; i++) {
         for (var j = l; j < l + 3; j++) {
             quad.push(gameArr[i][j]);
         }
     }
-
+// this loop is working for replacing value from puzzle.
     for (var n = 1; n < 10; n++) {
         if (row.indexOf(n) === -1 && col.indexOf(n) === -1 && quad.indexOf(n) === -1) {
             possArr.push(n);
@@ -59,59 +62,59 @@ function possiblities(r, c, gameArr) {
     }
     return possArr;
 }
-function checkQuadrant(r, c, gameArr) {
-    var qudarantArr = [];
-    for (var i = r; i < r + 3; i++) {
-        for (var j = c; j < c + 3; j++) {
-            if (qudarantArr.indexOf(gameArr[i][j]) === -1 || gameArr[i][j] === 0) {
-                qudarantArr.push(gameArr[i][j]);
-            } else {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-function isValidSudoku(gameArr) {
-    if (!checkQuadrant(0, 0, gameArr)) return false;
-    if (!checkQuadrant(0, 3, gameArr)) return false;
-    if (!checkQuadrant(0, 6, gameArr)) return false;
+// function checkQuadrant(r, c, gameArr) {
+//     var qudarantArr = [];
+//     for (var i = r; i < r + 3; i++) {
+//         for (var j = c; j < c + 3; j++) {
+//             if (qudarantArr.indexOf(gameArr[i][j]) === -1 || gameArr[i][j] === 0) {
+//                 qudarantArr.push(gameArr[i][j]);
+//             } else {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+// function isValidSudoku(gameArr) {
+//     if (!checkQuadrant(0, 0, gameArr)) return false;
+//     if (!checkQuadrant(0, 3, gameArr)) return false;
+//     if (!checkQuadrant(0, 6, gameArr)) return false;
 
-    if (!checkQuadrant(3, 0, gameArr)) return false;
-    if (!checkQuadrant(3, 3, gameArr)) return false;
-    if (!checkQuadrant(3, 6, gameArr)) return false;
+//     if (!checkQuadrant(3, 0, gameArr)) return false;
+//     if (!checkQuadrant(3, 3, gameArr)) return false;
+//     if (!checkQuadrant(3, 6, gameArr)) return false;
 
-    if (!checkQuadrant(6, 0, gameArr)) return false;
-    if (!checkQuadrant(6, 3, gameArr)) return false; 
-    if (!checkQuadrant(6, 6, gameArr)) return false;
+//     if (!checkQuadrant(6, 0, gameArr)) return false;
+//     if (!checkQuadrant(6, 3, gameArr)) return false; 
+//     if (!checkQuadrant(6, 6, gameArr)) return false;
 
-    for (var i = 0; i < gameArr.length; i++) {
-        var rowNumbers = [];
-        for (var j = 0; j < gameArr.length; j++) {
-            if (rowNumbers.indexOf(gameArr[i][j]) === -1 || gameArr[i][j] === 0) {
-                rowNumbers.push(gameArr[i][j]);
-            } else {
-                return false;
-            }
-        }
-    }
+//     for (var i = 0; i < gameArr.length; i++) {
+//         var rowNumbers = [];
+//         for (var j = 0; j < gameArr.length; j++) {
+//             if (rowNumbers.indexOf(gameArr[i][j]) === -1 || gameArr[i][j] === 0) {
+//                 rowNumbers.push(gameArr[i][j]);
+//             } else {
+//                 return false;
+//             }
+//         }
+//     }
 
-    for (var i = 0; i < gameArr.length; i++) {
-        var colNumbers = [];
-        for (var j = 0; j < gameArr.length; j++) {
-            if (colNumbers.indexOf(gameArr[j][i]) === -1 || gameArr[j][i] === 0) {
-                colNumbers.push(gameArr[j][i]);
-            } else {
-                return false;
-            }
-        }
-    }
-    return true;
-}
+//     for (var i = 0; i < gameArr.length; i++) {
+//         var colNumbers = [];
+//         for (var j = 0; j < gameArr.length; j++) {
+//             if (colNumbers.indexOf(gameArr[j][i]) === -1 || gameArr[j][i] === 0) {
+//                 colNumbers.push(gameArr[j][i]);
+//             } else {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
 
 //---------------------------------------------------Test-----------------------------------------------------------------
 
-
+// this is Array in this Array i am storing the puzzle data. 
 var gameArr = [
         [0,0,0,2,6,0,7,0,1],
         [6,8,0,0,7,0,0,9,0],
@@ -125,7 +128,6 @@ var gameArr = [
 ];
 
 solveSudoku(gameArr);
-
 console.log('Solved');
 console.log(gameArr);
-console.log(isValidSudoku(gameArr));
+// console.log(isValidSudoku(gameArr));
